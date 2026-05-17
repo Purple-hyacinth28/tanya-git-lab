4fe9cb962d64a9ab4d08b69007239007ace6636e
Purple-hyacinth28 <Tanya28102007@yandex.ru>
2026-05-18 01:40:55 +0300

    Fix vite.config.js for library mode

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/math.js'),
+      name: 'MathLib',
+      fileName: (format) => `math-lib.${format}.js`,
+    },
     outDir: 'dist',
   },
 });
